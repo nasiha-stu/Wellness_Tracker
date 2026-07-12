7366abb4356f4ab411851183f73d8bba73b3d924
nasiha-stu <nasiha.stu@gmail.com>
2026-07-12 17:10:52 +0200

    Create Habit model and initial migration

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Habit(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    frequency = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
