4e51303d6d741e19d1c3f2b141f180c3c56caf27
nasiha-stu <nasiha.stu@gmail.com>
2026-07-15 22:26:35 +0200

    Complete user authentication flow with logout

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    
     path("", views.home, name="home"),
+    path("register/", views.register, name="register"),
+    path("login/", views.user_login, name="login"),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("logout/", views.user_logout, name="logout"),
+
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 
 def home(request):
     return render(request, "tracker/home.html")
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("dashboard")
+
+    else:
+        form = UserCreationForm()
+
+    return render(request, "tracker/register.html", {"form": form})
+
+def user_login(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect("dashboard")
+
+    return render(request, "tracker/login.html")
+
+@login_required
+def dashboard(request):
+    return render(request, "tracker/dashboard.html")
+
+def user_logout(request):
+    logout(request)
+    return redirect("home")
